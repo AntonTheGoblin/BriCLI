1c78e9304516b59c3cae3244c946ab6bbb16c716
Anthony Wall <anthonyw@bytesnap.co.uk>
2025-01-07 15:19:52 +0000

    Add BricliError_t type

--- inc/bricli/bricli.h
+++ inc/bricli/bricli.h
@@ -9,6 +9,16 @@
 #endif // BRICLI_BUFFER_SIZE
 
 // Includes, types and defines
+/**
+ * Contains error types for use with the BriCLI library
+ */
+typedef enum _BricliError_t
+{
+	BricliErrorOk,				// 0: Success
+	BricliErrorUnknown,			// 1: An unexpected error occurred
+	BricliErrorNullArgument,	// 2: A NULL value was incorrectly given as an argument
+	BricliErrorInavlidArugment	// 3: An invalid parameter was given as an argument
+} BricliError_t;
 
 #ifdef __cplusplus
 extern "C" {
