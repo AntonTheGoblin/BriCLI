de26702fc7a050605770f4bf6e3d88a9407d6484
Anthony Wall <tj@wall5.co.uk>
2025-03-25 09:02:42 +0000

    Add BspWrite implementation

--- inc/bricli/api.h
+++ inc/bricli/api.h
@@ -13,6 +13,8 @@
 #ifndef BRICLI_API_H
 #define BRICLI_API_H
 
+#include <stddef.h>
+#include <string.h>
 #include <bricli/types.h>
 
 #ifdef __cplusplus
@@ -21,6 +23,55 @@ extern "C" {
 
 	BricliError_t Bricli_Init(Bricli_t *cli, BricliInit_t *init);
 	BricliError_t Bricli_Parse(uint8_t *buffer, uint32_t length);
+
+
+/**
+* @brief Helper function for writing out data on the CLI's write function.
+*
+* @param cli Pointer to the CLI instance to use.
+* @param length The number of characters in the buffer to be sent.
+* @param data Pointer to the buffer to be sent.
+*
+* @return An error code, negative indicates a problem occurred.
+*/
+static inline int BricliWrite(Bricli_t* cli, uint32_t length, const uint8_t* data)
+{
+    // Make sure we actually have a write function.
+    if (cli != NULL && cli->BspWrite != NULL)
+    {
+        return cli->BspWrite(length, data);
+    }
+    else
+    {
+        cli->LastError = BricliErrorInternal;
+        return BricliErrorBadHandle;
+    }
+}
+
+/**
+ * @brief Helper function for writing out data with automatic EOL appending.
+ * 
+ * @param cli Pointer to the CLI instance to use.
+ * @param length The number of characters in the buffer to be sent.
+ * @param data Pointer to the buffer to be sent.
+ * 
+ * @return An error code, negative indicates a problem occurred. 
+ */
+static inline int BricliWriteLine(Bricli_t* cli, uint32_t length, const uint8_t* data)
+{
+    BricliWrite(cli, length, data);
+
+    if (cli->TxEol == NULL)
+    {
+        return BricliWrite(cli, strlen((const char*)cli->RxEol), cli->RxEol);
+    }
+    else
+    {
+        return BricliWrite(cli, strlen((const char*)cli->TxEol), cli->TxEol);
+    }
+    
+}
+
 
 #ifdef __cplusplus
 }
