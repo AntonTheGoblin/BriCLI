a5914072bf9de5c2beeec39cdab0df39c1b7eddf
Anthony Wall <tj@wall5.co.uk>
2025-10-02 06:49:49 +0100

    Remove command header

--- Include/bricli/handle.h
+++ Include/bricli/handle.h
@@ -3,7 +3,6 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-#include "command.h"
 #include "error.h"
 
 /**
@@ -35,6 +34,35 @@ typedef void (*Bricli_StateChanged)(BricliStates_t oldState, BricliStates_t newS
  * @param data      The data to be written.
  */
 typedef int (*Bricli_BspWrite)(uint32_t length, const char* data);
+
+/**
+ * @brief Command handler function, one should be provided for every command.
+ *
+ * @param numberOfArgs The number of arguments found.
+ * @param args         Array of argument string found.
+ */
+typedef int (*Bricli_CommandHandler)(uint32_t numberOfArgs, char* args[]);
+
+/**
+ * @brief Holds specific details for a command entry used by this CLI.
+ *
+ * @param                       The command name.
+ * @param Handler               Handler function for this command.
+ * @param HelpMessage           Optional message to display with the built-in help command.
+ * @param AuthScopesRequired    The authorization scopes needed to run this command, 0 enables access to all users
+ */
+typedef struct _BricliCommand_t
+{
+    const char*             Name;                   /*<< Command name. */
+    Bricli_CommandHandler   Handler;                /*<< Handler function for this command. */
+    const char*             HelpMessage;            /*<< Optional message to be displayed by the help command. */
+    BricliAuthScopes_t      AuthScopesRequired;     /*<< The authorisation scopes required to access this command. */
+} BricliCommand_t;
+
+/**
+ * @brief Terminator for marking the end of a BriCLI command list
+ */
+#define BRICLI_COMMAND_LIST_TERMINATOR {NULL, NULL, NULL, 0}
 
 /**
  * @brief Initialistion options for a BriCLI instance
@@ -85,6 +113,7 @@ extern "C" {
 #endif // __cplusplus
 
 extern BricliErrors_t Bricli_Init(BricliHandle_t *cli, const BricliInit_t *settings);
+extern void Bricli_ClearCommand(BricliHandle_t *cli);
 
 #ifdef __cplusplus
 }
